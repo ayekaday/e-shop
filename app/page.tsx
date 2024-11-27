a837627a3bb0fa54e74cb1fdb6871775f91670a7
ayekaday <yukiakd279@gmail.com>
2024-11-27 10:04:32 +0630

    finit banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import HomeBanner from "./components/nav/HomeBanner";
+import Container from "./Container";
+
 export default function Home() {
-  return <div>E-Shop</div>;
+  return (
+    <div>
+      <Container>
+        <HomeBanner />
+      </Container>
+    </div>
+  );
 }
