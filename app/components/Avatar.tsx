fb25f15f13fe7854fc82526f9d9abecb2e72d9d6
ayekaday <yukiakd279@gmail.com>
2024-12-02 13:03:22 +0630

    Update Avatar

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -7,13 +7,15 @@ interface AvatarProps {
 
 const Avatar: React.FC<AvatarProps> = ({ src }) => {
   if (src) {
-    <Image
-      src={src}
-      alt="Avatar"
-      className="rounded-full"
-      height="30"
-      width="30"
-    />;
+    return (
+      <Image
+        src={src}
+        alt="Avatar"
+        className="rounded-full"
+        height="30"
+        width="30"
+      />
+    );
   }
   return <FaUserCircle size={24} />;
 };
