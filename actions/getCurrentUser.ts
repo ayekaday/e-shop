38b4bd90154dad4e259b97d7764ac7c3dec7f2c5
ayekaday <yukiakd279@gmail.com>
2024-12-11 13:21:46 +0630

    Update Logout link

--- actions/getCurrentUser.ts
+++ actions/getCurrentUser.ts
@@ -23,7 +23,7 @@ export async function getCurrentUser() {
     return {
       ...currentUser,
       createdAt: currentUser.createdAt.toISOString(),
-      updateAt: currentUser.updatedAt.toISOString(),
+      updateAt: currentUser.updateAt.toISOString(),
       emailVerified: currentUser.emailVerified?.toString() || null,
     };
   } catch (error: any) {
