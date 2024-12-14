fa5070c2a359159750879bed5d89f49dee2ef974
ayekaday <yukiakd279@gmail.com>
2024-12-14 08:28:25 +0630

    CheckoutClient

---
+++ app/checkout/page.tsx
@@ -0,0 +1,14 @@
+import Container from "../components/Container";
+import FormWrap from "../components/FormWrap";
+
+const Checkout = () => {
+  return (
+    <div className="p-8">
+      <Container>
+        <FormWrap>Checkout</FormWrap>
+      </Container>
+    </div>
+  );
+};
+
+export default Checkout;
